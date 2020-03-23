cb0bf47fd9f831fec09e52e9bdfc3a265f58936d
OlegTripolskiy <bizprosto@gmail.com>
2020-03-23 08:42:51 +0200

    0.0.3.0. Add plugin Touch Punch for touch device

--- js/proba.js
+++ js/proba.js
@@ -1,2 +1,15 @@
-// Создал файл, чтобы появилась папка на GitHub
-alert("proba.js WORK"); // 
+//alert("PROBA JS");
+/* Событие load на объекте window наступает, когда загрузилась вся страница, включая стили, картинки и другие ресурсы.
+*/
+window.onload = function() {
+  alert("PAGE DOWNLOAD.");
+  $("#slider-01 > span").on('click', function() {
+    alert("CLICK by handle!");
+  });
+};
+/*
+$("#slider-01").on('click', function() {
+    alert("CLICK");
+});
+$("ui-slider .ui-slider-handle").css("background-color", 'green');
+*/
